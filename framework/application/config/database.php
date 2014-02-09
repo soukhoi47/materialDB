a843f45a37e6c885108a90a0d12aa4052d1ba26a
Alexandre Aury <alex.bere288@gmail.com>
2014-02-09 18:30:01 +0100

    config initiale
    modification de autoload.php et database.php

--- framework/application/config/database.php
+++ framework/application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'dev_materialDB';
+$db['default']['password'] = 'dev_materialDB';
+$db['default']['database'] = 'dev_materialDB';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
