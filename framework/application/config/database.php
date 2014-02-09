46935d52e802c5485abe8f50784b1fe040ba6e55
Alexandre Aury <alex.bere288@gmail.com>
2014-02-09 18:31:56 +0100

    tt

--- framework/application/config/database.php
+++ framework/application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'dev_materialDB';
-$db['default']['password'] = 'dev_materialDB';
+$db['default']['password'] = 'Valence123';
 $db['default']['database'] = 'dev_materialDB';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
