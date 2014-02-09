8165d0c178333649912f10994178c159106970c8
Alexandre Aury <alex.bere288@gmail.com>
2014-02-09 18:37:29 +0100

    tt

--- framework/application/config/database.php
+++ framework/application/config/database.php
@@ -49,7 +49,7 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'dev_materialDB';
+$db['default']['username'] = 'materialDB';
 $db['default']['password'] = 'Valence123';
 $db['default']['database'] = 'dev_materialDB';
 $db['default']['dbdriver'] = 'mysql';
