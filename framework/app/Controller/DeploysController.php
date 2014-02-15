a47b67c8d729770bf133fbd7d39b3eeeb84355fb
Alexandre Aury <alex.bere288@gmail.com>
2014-02-15 17:47:48 +0100

    adding automatic deploy functionality from github and bitbucket

---
+++ framework/app/Controller/DeploysController.php
@@ -0,0 +1,35 @@
+<?php
+class PostsController extends AppController {
+    public $helpers = array('Html', 'Form');
+
+    public function index() {
+    		/**
+	 * GIT DEPLOYMENT SCRIPT
+	 *
+	 * Used for automatically deploying websites via github or bitbucket
+	 *
+	 */
+ 
+	// The commands
+	$commands = array(
+		'cd /home/alexandre/Documents/materialDB'
+		'echo $PWD',
+		'whoami',
+		'git pull',
+		'git status',
+		'git submodule sync',
+		'git submodule update',
+		'git submodule status',
+	);
+ 
+	// Run the commands for output
+	$output = '';
+		foreach($commands AS $command){
+			// Run it
+			$tmp = shell_exec($command);
+			// Output
+			$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
+			$output .= htmlentities(trim($tmp)) . "\n";
+		}
+    }
+}
--- framework/app/Model/Post.php
+++ framework/app/Model/Post.php
@@ -1,4 +1,4 @@
 <?php
 class Post extends AppModel {
-
+	
 }
