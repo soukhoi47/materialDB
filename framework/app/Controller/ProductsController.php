6f34531e93414ef87323053927de97b091bc323d
Alexandre Aury <alex.bere288@gmail.com>
2014-02-21 23:03:37 +0100

    tt

--- framework/app/Controller/ProductsController.php
+++ framework/app/Controller/ProductsController.php
@@ -3,7 +3,7 @@ class ProductsController extends AppController {
     public $helpers = array('Html', 'Form');
 /*Index controller listing all Products*/
     public function index() {
-        $this->set('products', $this->Product->find('all'));
+        $this->set('Products', $this->Product->find('all'));
     }
 
 }
