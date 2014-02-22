c54b5ecde5ffa8ab94cc29a39e5c854c818c527d
Alexandre Aury <alex.bere288@gmail.com>
2014-02-22 22:09:55 +0100

    tt

--- framework/app/Controller/ProductsController.php
+++ framework/app/Controller/ProductsController.php
@@ -4,8 +4,19 @@ class ProductsController extends AppController {
 /*Index controller listing all Products*/
     public function index() {
         $this->set('Products', $this->Product->find('all'));
-        debug($this);
-        die();
+    }
+
+/*view funtion to control the view of a product*/
+     public function view($id = null) {
+        if (!$id) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+
+        $post = $this->Product->findById($id);
+        if (!$product) {
+            throw new NotFoundException(__('Invalid post'));
+        }
+        $this->set('product', $product);
     }
 
 }
---
+++ framework/app/View/Products/view.ctp
@@ -0,0 +1,5 @@
+<!-- File: /app/View/Products/view.ctp -->
+
+<h1><?php echo h($post['Product']['name']); ?></h1>
+
+<p><small>Created: <?php echo $post['Product']['created']; ?></small></p>
